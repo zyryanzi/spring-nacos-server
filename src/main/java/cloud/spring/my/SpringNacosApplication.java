ebb0852d5c6c98b0dbf32b1954bac93c3df25740
zhouyurui <zhouyurui@symdata.cn>
2022-02-23 14:39:56 +0800

    ignore

--- src/main/java/cloud/spring/my/SpringNacosApplication.java
+++ src/main/java/cloud/spring/my/SpringNacosApplication.java
@@ -2,16 +2,21 @@ package cloud.spring.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App
+@EnableDiscoveryClient
+public class SpringNacosApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringNacosApplication.class, args);
+        String domain = applicationContext.getEnvironment().getProperty("beyound.domain");
+        System.out.println("spring is running on cloud --->>> " + domain);
     }
 }
